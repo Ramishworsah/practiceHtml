dc66ecfed893e4cc1b5a4553cd17b041eac025ad
Ramishwor sah <122091424+Ramishworsah@users.noreply.github.com>
2025-01-21 06:24:58 +0530

    done

--- first.js
+++ first.js
@@ -1,11 +1,21 @@
-//String
-let str="hello world"  //*it can not change the original value 
-console.log(str.replace("hello","welcome"));
-console.log(str);
+// //String
+// let str="hello world"  //*it can not change the original value 
+// console.log(str.replace("hello","welcome"));
+// console.log(str);
 
-//at,charat,lenght ,lastindex,indexof, charCodeAt,substring,Array.from(string variable);
-let arr=Array.from(str);
-let maparr=arr.map((Element,index)=>{
-console.log( `${Element}-${index}`)
+// //at,charat,lenght ,lastindex,indexof, charCodeAt,substring,Array.from(string variable);
+// let arr=Array.from(str);
+// let maparr=arr.map((Element,index)=>{
+// console.log( `${Element}-${index}`)
 
-})
+// })
+let str="hellow mohan lala";
+let strArr=Array.from(str);
+let sring=(arr)=>{
+    
+let vowelLetter='aeiuo'
+for(let i of vowelLetter){
+    console.log(str.includes(i));
+}
+}
+sring(str);
